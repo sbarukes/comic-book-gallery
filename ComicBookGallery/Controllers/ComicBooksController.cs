d087a1e898bf71642d9b9ff08961723f42abd44c
sbarukes <sbarukes@gmail.com>
2018-04-19 19:58:13 -0400

    added comic books list view

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -17,6 +17,13 @@ namespace ComicBookGallery.Controllers
             _comicBookRepository = new ComicBookRepository();
         }
 
+        public ActionResult Index()
+        {
+            var comicbooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicbooks);
+        }
+
         public ActionResult Detail(int? id)
         {
             if (id == null)
